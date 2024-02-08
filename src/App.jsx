745d145927c05b4c0366cc78da0c2667b62cf7e6
alok_mishra14 <143alokmishra143@gmail.com>
2024-02-08 23:17:36 +0530

    adding the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="w-full h-screen  text-white">
+      <Navbar />
+      <LandingPage />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  const links = ["Service", "Our Works", "About us", "Insights", "Contact us"];
+
+  return (
+    <div className="fixed z-[999] px-20 w-full py-8 font-[ 'Neue_Montreal_Regular'] flex justify-between items-center">
+      <div className="logo">
+        <img src="./ochi.svg" alt="ochi" />
+      </div>
+
+      <div className="links flex gap-10">
+        {links.map((item, index) => (
+          <a
+            href="#"
+            key={index}
+            className={`capitalize text-lg font-light ${
+              index === 4 ? "ml-32" : ""
+            }`}
+          >
+            {item}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
